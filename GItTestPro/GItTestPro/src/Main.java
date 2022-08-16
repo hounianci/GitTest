906f957754f8f75eac22507ae3b18a451b01e0b0
hounianci <762558614@qq.com>
2022-08-16 11:57:46 +0800

    b3e2

--- GItTestPro/GItTestPro/src/Main.java
+++ GItTestPro/GItTestPro/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("12");
         System.out.println("branch3");
+        System.out.println("b3e2");
     }
 }
