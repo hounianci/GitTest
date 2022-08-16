0552242b8fa38253619e45a5f23fa86cc5628c87
hounianci <762558614@qq.com>
2022-08-16 17:48:32 +0800

    m

--- GItTestPro/GItTestPro/src/Main.java
+++ GItTestPro/GItTestPro/src/Main.java
@@ -12,6 +12,8 @@ public class Main {
         System.out.println("2");
         System.out.println("1");
         System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
         System.out.println("7");
     }
 
